fd80c06df590d9434081bd815e62344f3de62d00
Spencer Cherry <spencer.cherry@yahoo.com>
2017-12-01 21:20:46 -0500

    Add URLs to config.

--- assets/wp-config.php
+++ assets/wp-config.php
@@ -26,34 +26,15 @@ define('DBI_AWS_SECRET_ACCESS_KEY', $_SERVER['AWS_SECRET_ACCESS_KEY']);
 define('WP_HOME', $_SERVER['WP_HOME']);
 define('WP_SITEURL', $_SERVER['WP_SITEURL']);
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
+// MySQL settings.
 define('DB_NAME', $_SERVER['RDS_DB_NAME']);
-
-/** MySQL database username */
 define('DB_USER', $_SERVER['RDS_USERNAME']);
-
-/** MySQL database password */
 define('DB_PASSWORD', $_SERVER['RDS_PASSWORD']);
-
-/** MySQL hostname */
 define('DB_HOST', $_SERVER['RDS_HOSTNAME']);
-
-/** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
+// Authentication keys and salts.
 define('AUTH_KEY',         $_SERVER['WP_AUTH_KEY']);
 define('SECURE_AUTH_KEY',  $_SERVER['WP_SECURE_AUTH_KEY']);
 define('LOGGED_IN_KEY',    $_SERVER['WP_LOGGED_IN_KEY']);
