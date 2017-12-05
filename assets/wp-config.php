c60edacc8644f3483f420e6237f7ed5a7782ecc7
Spencer Cherry <spencer.cherry@yahoo.com>
2017-12-05 09:18:10 -0500

    Fix typo in wp-config.php.

--- assets/wp-config.php
+++ assets/wp-config.php
@@ -1,22 +1,4 @@
 <?php
-/**
- * The base configuration for WordPress
- *
- * The wp-config.php creation script uses this file during the
- * installation. You don't have to use the web site, you can
- * copy this file to "wp-config.php" and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * MySQL settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://codex.wordpress.org/Editing_wp-config.php
- *
- * @package WordPress
- */
 
 // AWS S3 access.
 define('DBI_AWS_ACCESS_KEY_ID', $_SERVER['AWS_ACCESS_KEY_ID']);
@@ -30,7 +12,7 @@ define('WP_SITEURL', $_SERVER['WP_SITEURL']);
 define('DB_NAME', $_SERVER['RDS_DB_NAME']);
 define('DB_USER', $_SERVER['RDS_USERNAME']);
 define('DB_PASSWORD', $_SERVER['RDS_PASSWORD']);
-define('DB_HOST', $_SERVER['RDS_HOSTNAME']);
+define('DB_HOST', $_SERVER['RDS_HOSTNAME'] . ':' . $_SERVER['RDS_PORT']);
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
@@ -44,35 +26,8 @@ define('SECURE_AUTH_SALT', $_SERVER['WP_SECURE_AUTH_SALT']);
 define('LOGGED_IN_SALT',   $_SERVER['WP_LOGGED_IN_SALT']);
 define('NONCE_SALT',       $_SERVER['WP_NONCE_SALT']);
 
-/**#@-*/
-
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
 $table_prefix  = 'wp_';
-
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the Codex.
- *
- * @link https://codex.wordpress.org/Debugging_in_WordPress
- */
 define('WP_DEBUG', false);
-
-/* That's all, stop editing! Happy blogging. */
-
-/** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
-
-/** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
